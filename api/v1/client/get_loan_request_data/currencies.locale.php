bd7e9dd92a37f3a6ab4af6c41bdf81386f6f0dd9
Henri Toivar <henri.toivar@gmail.com>
2015-05-11 16:07:50 +0300

    Folder structure change.

---
+++ api/v1/client/get_loan_request_data/currencies.locale.php
@@ -0,0 +1,496 @@
+<?php
+ return array (
+  '3month' => 
+  (object)(array(
+     '1' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '2' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '3' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '4' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '5' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '6' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '7' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '8' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '9' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '10' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '11' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '12' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '13' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '14' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '15' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '16' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '17' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '18' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '19' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '20' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '21' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '22' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '23' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '24' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '25' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '26' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '27' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '28' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '29' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '30' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '31' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '32' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '33' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '34' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '35' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '36' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '37' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '38' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '39' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '40' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '41' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 425,
+    )),
+     '42' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 450,
+    )),
+     '43' => 
+    (object)(array(
+       'lower' => 100,
+       'upper' => 450,
+    )),
+     '44' => 
+    (object)(array(
+       'lower' => 125,
+       'upper' => 450,
+    )),
+     '45' => 
+    (object)(array(
+       'lower' => 125,
+       'upper' => 475,
+    )),
+     '46' => 
+    (object)(array(
+       'lower' => 125,
+       'upper' => 475,
+    )),
+     '47' => 
+    (object)(array(
+       'lower' => 125,
+       'upper' => 475,
+    )),
+     '48' => 
+    (object)(array(
+       'lower' => 125,
+       'upper' => 500,
+    )),
+     '49' => 
+    (object)(array(
+       'lower' => 150,
+       'upper' => 500,
+    )),
+     '50' => 
+    (object)(array(
+       'lower' => 150,
+       'upper' => 500,
+    )),
+     '51' => 
+    (object)(array(
+       'lower' => 150,
+       'upper' => 525,
+    )),
+     '52' => 
+    (object)(array(
+       'lower' => 150,
+       'upper' => 525,
+    )),
+     '53' => 
+    (object)(array(
+       'lower' => 175,
+       'upper' => 525,
+    )),
+     '54' => 
+    (object)(array(
+       'lower' => 175,
+       'upper' => 550,
+    )),
+     '55' => 
+    (object)(array(
+       'lower' => 175,
+       'upper' => 550,
+    )),
+     '56' => 
+    (object)(array(
+       'lower' => 175,
+       'upper' => 550,
+    )),
+     '57' => 
+    (object)(array(
+       'lower' => 175,
+       'upper' => 575,
+    )),
+     '58' => 
+    (object)(array(
+       'lower' => 200,
+       'upper' => 575,
+    )),
+     '59' => 
+    (object)(array(
+       'lower' => 200,
+       'upper' => 575,
+    )),
+     '60' => 
+    (object)(array(
+       'lower' => 200,
+       'upper' => 600,
+    )),
+     '61' => 
+    (object)(array(
+       'lower' => 200,
+       'upper' => 600,
+    )),
+     '62' => 
+    (object)(array(
+       'lower' => 225,
+       'upper' => 600,
+    )),
+     '63' => 
+    (object)(array(
+       'lower' => 225,
+       'upper' => 625,
+    )),
+     '64' => 
+    (object)(array(
+       'lower' => 225,
+       'upper' => 625,
+    )),
+     '65' => 
+    (object)(array(
+       'lower' => 225,
+       'upper' => 625,
+    )),
+     '66' => 
+    (object)(array(
+       'lower' => 225,
+       'upper' => 650,
+    )),
+     '67' => 
+    (object)(array(
+       'lower' => 250,
+       'upper' => 650,
+    )),
+     '68' => 
+    (object)(array(
+       'lower' => 250,
+       'upper' => 650,
+    )),
+     '69' => 
+    (object)(array(
+       'lower' => 250,
+       'upper' => 675,
+    )),
+     '70' => 
+    (object)(array(
+       'lower' => 250,
+       'upper' => 675,
+    )),
+     '71' => 
+    (object)(array(
+       'lower' => 275,
+       'upper' => 675,
+    )),
+     '72' => 
+    (object)(array(
+       'lower' => 275,
+       'upper' => 700,
+    )),
+     '73' => 
+    (object)(array(
+       'lower' => 275,
+       'upper' => 700,
+    )),
+     '74' => 
+    (object)(array(
+       'lower' => 275,
+       'upper' => 700,
+    )),
+     '75' => 
+    (object)(array(
+       'lower' => 275,
+       'upper' => 725,
+    )),
+     '76' => 
+    (object)(array(
+       'lower' => 300,
+       'upper' => 725,
+    )),
+     '77' => 
+    (object)(array(
+       'lower' => 300,
+       'upper' => 725,
+    )),
+     '78' => 
+    (object)(array(
+       'lower' => 300,
+       'upper' => 750,
+    )),
+     '79' => 
+    (object)(array(
+       'lower' => 300,
+       'upper' => 750,
+    )),
+     '80' => 
+    (object)(array(
+       'lower' => 325,
+       'upper' => 750,
+    )),
+     '81' => 
+    (object)(array(
+       'lower' => 325,
+       'upper' => 775,
+    )),
+     '82' => 
+    (object)(array(
+       'lower' => 325,
+       'upper' => 775,
+    )),
+     '83' => 
+    (object)(array(
+       'lower' => 325,
+       'upper' => 775,
+    )),
+     '84' => 
+    (object)(array(
+       'lower' => 325,
+       'upper' => 800,
+    )),
+     '85' => 
+    (object)(array(
+       'lower' => 350,
+       'upper' => 800,
+    )),
+     '86' => 
+    (object)(array(
+       'lower' => 350,
+       'upper' => 800,
+    )),
+     '87' => 
+    (object)(array(
+       'lower' => 350,
+       'upper' => 825,
+    )),
+     '88' => 
+    (object)(array(
+       'lower' => 350,
+       'upper' => 825,
+    )),
+     '89' => 
+    (object)(array(
+       'lower' => 375,
+       'upper' => 825,
+    )),
+     '90' => 
+    (object)(array(
+       'lower' => 375,
+       'upper' => 850,
+    )),
+     '91' => 
+    (object)(array(
+       'lower' => 375,
+       'upper' => 850,
+    )),
+     '92' => 
+    (object)(array(
+       'lower' => 375,
+       'upper' => 850,
+    )),
+     '93' => 
+    (object)(array(
+       'lower' => 375,
+       'upper' => 875,
+    )),
+     '94' => 
+    (object)(array(
+       'lower' => 400,
+       'upper' => 875,
+    )),
+     '95' => 
+    (object)(array(
+       'lower' => 400,
+       'upper' => 875,
+    )),
+     '96' => 
+    (object)(array(
+       'lower' => 400,
+       'upper' => 900,
+    )),
+     '97' => 
+    (object)(array(
+       'lower' => 400,
+       'upper' => 900,
+    )),
+     '98' => 
+    (object)(array(
+       'lower' => 425,
+       'upper' => 925,
+    )),
+  )),
+);
